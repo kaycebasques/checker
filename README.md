240b8aa8a2f25fd63bfc7562726628e2f8649563
Kayce Basques <kayce@google.com>
2020-12-03 20:50:44 -0800

    update README

--- README.md
+++ README.md
@@ -1,103 +1,13 @@
-# docs-migration-tool
+# checker
 
-An automated tool for converting web pages to Markdown.
+A link and ID checker.
 
-## Prerequisite knowledge
+## Usage
 
-This tool requires basic knowledge of JavaScript, HTML, git, and command line interfaces.
+1. Put the pages you want to check in `targets.txt`.
+   `all.txt` contains all of the old site's URLs, except 
+   `https://developer.chrome.com` has been replaced with `http://localhost:8080`.
+1. Run `npm i`.
+1. Run `npm run check`.
 
-## Assumptions
-
-This tool has only been developed and used on macOS Catalina.
-
-## Features
-
-### Remove nodes declaratively
-
-### Modify nodes flexibly
-
-### Save partial progress
-
-Suppose you need to migrate 100 pages. You run the tool
-but your screen goes to sleep, or your Wi-Fi fails halfway through.
-Set `history` to `true` in `config.json` to instruct the tool to
-pick up where you left off.
-
-### Copy images
-
-## Get started
-
-### Installation
-
-1. [Install nvm][nvm].
-
-1. Use nvm to install Node.js version 12.18.4:
-
-       nvm install 12.18.4
-
-1. Use nvm to set 12.18.4 as your active version:
-
-       nvm use 12.18.4
-
-   You can optionally set version 12.18.4 as your default:
-
-       nvm alias default 12.18.4
-
-1. Clone this repository:
-
-       git clone git@github.com:kaycebasques/docs-migration-tool.git
-
-1. Set your working directory to the repository:
-
-       cd path/to/docs-migration-tool
-
-1. Install NPM packages:
-
-       npm install
-
-### Setup
-
-1. Add the list of URLs that you want to migrate to `targets.txt`:
-
-       https://developer.chrome.com/extensions
-       https://developer.chrome.com/extensions/getstarted
-       https://developer.chrome.com/extensions/overview
-
-1. Open `config.json`.
-
-1. Set the `selector` property to the DOM selector of the main content:
-
-       "selector": "div[itemprop=\"articleBody\"]"
-
-1. Use the `deletions` property to specify any nodes that should be deleted.
-   Each item in the array should be a DOM selector:
-
-       "deletions": [
-         "nav.inline-toc",
-         "a.permalink"
-       ]
-
-1. Use the `modifications` property to specify a path to a JavaScript file
-   that will modify each page before converting to Markdown:
-
-       "modifications": "modifications/dcc.js"
-
-1. Create your modification script (e.g. `modifications/dcc.js`) and add the modification
-   code:
-
-       // Wrap each <pre class="prettyprint"> element in a <code> element
-       // so that the Markdown converting tool formats them properly as codeblocks.
-       document.querySelectorAll('pre.prettyprint').forEach(target => {
-         const code = document.createElement('code');
-         code.innerHTML = target.innerHTML;
-         target.innerHTML = '';
-         target.appendChild(code);
-       });
-
-### Usage
-
-1. Run the conversion script:
-
-       npm run convert
-
-[nvm]: https://github.com/nvm-sh/nvm#installing-and-updating
+The results will be written to `report.txt`.
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const readline = require('readline');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const TurndownService = require('turndown');
-const axios = require('axios');
-let outputDirectory = 'output';
+// const TurndownService = require('turndown');
+// const axios = require('axios');
+// let outputDirectory = 'output';
 const events = require('events');
-const prettier = require('prettier');
+// const prettier = require('prettier');
 let config;
 
 function init() {
@@ -36,7 +36,7 @@ function init() {
     targets = Array.from(targetsSet);
     migrate(targets, done);
   });
-  if (config.output) outputDirectory = config.output;
+  // if (config.output) outputDirectory = config.output;
   // TODO(kaycebasques): How do we handle this when the user has indicated that
   // they want to save progress? Should we not delete the output directory?
   // fs.rmdirSync(outputDirectory, {recursive: true});
@@ -65,39 +65,39 @@ function init() {
   }
 }
 
-async function download(image, destinationDirectory) {
-  const pathname = new URL(image).pathname;
-  const filename = pathname.substring(pathname.lastIndexOf('/') + 1);
-  const destination = `${destinationDirectory}/${filename}`;
-  try {
-    const response = await axios({
-      url: image,
-      method: 'GET',
-      responseType: 'stream'
-    });
-    const writer = fs.createWriteStream(destination);
-    response.data.pipe(writer);
-  } catch (error) {
-    console.error(`Error while attempting to download ${image}`);
-    return null;
-  }
-}
+// async function download(image, destinationDirectory) {
+//   const pathname = new URL(image).pathname;
+//   const filename = pathname.substring(pathname.lastIndexOf('/') + 1);
+//   const destination = `${destinationDirectory}/${filename}`;
+//   try {
+//     const response = await axios({
+//       url: image,
+//       method: 'GET',
+//       responseType: 'stream'
+//     });
+//     const writer = fs.createWriteStream(destination);
+//     response.data.pipe(writer);
+//   } catch (error) {
+//     console.error(`Error while attempting to download ${image}`);
+//     return null;
+//   }
+// }
 
-async function modify(page) {
-  if (!config || !config.modifications || !fs.existsSync(config.modifications)) return;
-  await page.addScriptTag({
-    path: config.modifications
-  });
-}
+// async function modify(page) {
+//   if (!config || !config.modifications || !fs.existsSync(config.modifications)) return;
+//   await page.addScriptTag({
+//     path: config.modifications
+//   });
+// }
 
-async function cleanup(page) {
-  if (!config || !config.deletions) return;
-  const selectors = config.deletions;
-  for (let i = 0; i < selectors.length; i++) {
-    const selector = selectors[i];
-    await page.$$eval(selector, nodes => nodes.forEach(node => node.remove()));
-  }
-}
+// async function cleanup(page) {
+//   if (!config || !config.deletions) return;
+//   const selectors = config.deletions;
+//   for (let i = 0; i < selectors.length; i++) {
+//     const selector = selectors[i];
+//     await page.$$eval(selector, nodes => nodes.forEach(node => node.remove()));
+//   }
+// }
 
 async function migrate(targets, done) {
   // TODO get the config if it exists and use its deletion/modification directions
@@ -108,94 +108,102 @@ async function migrate(targets, done) {
   });
   const page = await browser.newPage();
   // TODO move to init? And expose page as a global?
-  done = done.length > 0 ? `${done.join('\n')}` : '';
+  // done = done.length > 0 ? `${done.join('\n')}` : '';
+  let data = {
+    ok: [],
+    redirects: [],
+    '404s': []
+  };
   for (let i = 0; i < targets.length; i++) {
-    let frontmatter = 
-        '---\n' +
-        // TODO(kaycebasques): Need to configure this because it's DCC-specific.
-        `layout: 'layouts/doc-post.njk'\n`;
+    // let frontmatter = 
+    //     '---\n' +
+    //     // TODO(kaycebasques): Need to configure this because it's DCC-specific.
+    //     `layout: 'layouts/doc-post.njk'\n`;
     const target = targets[i];
-    console.info(`Scraping ${target}`);
-    const pathname = new URL(target).pathname;
+    console.info(`Checking ${target}`);
+    // const pathname = new URL(target).pathname;
     // const destination = `${outputDirectory}${pathname}`;
     // fs.mkdirSync(destination, {recursive: true});
     try {
-      await page.goto(target, {
-        waitUntil: 'networkidle0',
-        timeout: 0
+      const response = await page.goto(target, {
+        waitUntil: 'networkidle0'
       });
+      const url = await page.url();
+      if (url !== target) data.redirects.push({from: target, to: url});
+      if (response.status() === 404) data['404s'].push(target);
     } catch (error) {
       console.error(`Error visiting ${target}`);
       continue;
     }
+    fs.writeFileSync('report.txt', JSON.stringify(data, null, '  '));
     // Need to extract the title before cleanup() so that we don't have duplicate titles on the page
-    if (config.selectors.title) {
-      const title = await page.$eval(config.selectors.title, element => element.textContent);
-      frontmatter += `title: "${title}"\n`;
-    }
-    await cleanup(page);
-    await modify(page);
+    // if (config.selectors.title) {
+    //   const title = await page.$eval(config.selectors.title, element => element.textContent);
+    //   frontmatter += `title: "${title}"\n`;
+    // }
+    // await cleanup(page);
+    // await modify(page);
     // TODO move to config.json
-    const contentSelector = config.selectors.main;
-    let html;
-    try {
-      html = await page.$eval(contentSelector, element => element.innerHTML);
-    } catch (error) {
-      console.error('Main selector not found.');
-      continue;
-    }
-    if (config.selectors.date) {
-      // Added this because in the case of developers.google.com/web, we do a network request
-      // to fetch the creation date and insert that information into the page.
-      // TODO(kaycebasques): Just loop through all user-provided selectors and wait for them all?
-      await page.waitForSelector(config.selectors.date);
-      const date = await page.$eval(config.selectors.date, element => element.textContent);
-      frontmatter += `date: ${date}\n`;
-    } else {
-      frontmatter += `#date: TODO\n`;
-    }
-    if (config.selectors.update) {
-      await page.waitForSelector(config.selectors.update);
-      const update = await page.$eval(config.selectors.update, element => element.textContent);
-      frontmatter += `updated: ${update}\n`;
-    } else {
-      frontmatter += `#updated: TODO\n`;
-    }
-    if (config.selectors.authors) {
-      await page.waitForSelector(config.selectors.authors);
-      const authors = await page.$$eval(config.selectors.authors, nodes => nodes.map(node => node.textContent));
-      console.log({authors});
+    // const contentSelector = config.selectors.main;
+    // let html;
+    // try {
+    //   html = await page.$eval(contentSelector, element => element.innerHTML);
+    // } catch (error) {
+    //   console.error('Main selector not found.');
+    //   continue;
+    // }
+    // if (config.selectors.date) {
+    //   // Added this because in the case of developers.google.com/web, we do a network request
+    //   // to fetch the creation date and insert that information into the page.
+    //   // TODO(kaycebasques): Just loop through all user-provided selectors and wait for them all?
+    //   await page.waitForSelector(config.selectors.date);
+    //   const date = await page.$eval(config.selectors.date, element => element.textContent);
+    //   frontmatter += `date: ${date}\n`;
+    // } else {
+    //   frontmatter += `#date: TODO\n`;
+    // }
+    // if (config.selectors.update) {
+    //   await page.waitForSelector(config.selectors.update);
+    //   const update = await page.$eval(config.selectors.update, element => element.textContent);
+    //   frontmatter += `updated: ${update}\n`;
+    // } else {
+    //   frontmatter += `#updated: TODO\n`;
+    // }
+    // if (config.selectors.authors) {
+    //   await page.waitForSelector(config.selectors.authors);
+    //   const authors = await page.$$eval(config.selectors.authors, nodes => nodes.map(node => node.textContent));
+    //   console.log({authors});
       // frontmatter += `updated: ${update}\n`;
-    }
-    if (config.selectors.description) {
-      try {
-        await page.waitForSelector(config.selectors.description, {timeout: 3000});
-        const description = await page.$eval(config.selectors.description, element => element.textContent);
-        frontmatter += `updated: ${description}\n`;
-      } catch (error) {
-        console.error('Description element not found.');
-        frontmatter += `#description: TODO\n`;
-      }
-    }
+    // }
+    // if (config.selectors.description) {
+    //   try {
+    //     await page.waitForSelector(config.selectors.description, {timeout: 3000});
+    //     const description = await page.$eval(config.selectors.description, element => element.textContent);
+    //     frontmatter += `updated: ${description}\n`;
+    //   } catch (error) {
+    //     console.error('Description element not found.');
+    //     frontmatter += `#description: TODO\n`;
+    //   }
+    // }
     // try {
     //   const description = await page.$eval('meta[name="description"]', element => element.content);
     //   frontmatter += `description: ${description}\n`;
     // } catch (error) {
     //   console.error('Description element not found.');
     // }
-    const destination = `${outputDirectory}${pathname}`;
-    fs.mkdirSync(destination, {recursive: true});
-    const images = await page.$$eval(`${contentSelector} img`, images => images.map(image => image.src));
-    for (let i = 0; i < images.length; i++) {
-      await download(images[i], destination);
-    }
-    const turndownService = TurndownService({
-      headingStyle: 'atx',
-      codeBlockStyle: 'fenced',
-      linkStyle: 'referenced',
-      linkReferenceStyle: 'full'
-    });
-    turndownService.keep(['table']);
+    // const destination = `${outputDirectory}${pathname}`;
+    // fs.mkdirSync(destination, {recursive: true});
+    // const images = await page.$$eval(`${contentSelector} img`, images => images.map(image => image.src));
+    // for (let i = 0; i < images.length; i++) {
+    //   await download(images[i], destination);
+    // }
+    // const turndownService = TurndownService({
+    //   headingStyle: 'atx',
+    //   codeBlockStyle: 'fenced',
+    //   linkStyle: 'referenced',
+    //   linkReferenceStyle: 'full'
+    // });
+    // turndownService.keep(['table']);
     // TODO(kaycebasques): Refactor this DCC-specific code.
     // turndownService.keep(node => {
     //   return node.nodeName === 'DIV' && node.classList.contains('aside--note');
@@ -207,62 +215,62 @@ async function migrate(targets, done) {
     //   return node.nodeName === 'DIV' && node.classList.contains('aside--warning');
     // });
     // Not working. Can't get a newline after first !!! characters.
-    turndownService.addRule('notes', {
-      filter: node => {
-        return node.nodeName === 'P' && node.classList.contains('note');
-      },
-      replacement: (content, node) => {
-        return `!!!.aside.aside--note\n\n${content}\n\n!!!\n\n`
-      }
-    });
-    turndownService.addRule('cautions', {
-      filter: node => {
-        return node.nodeName === 'P' && node.classList.contains('caution');
-      },
-      replacement: (content, node) => {
-        return `!!!.aside.aside--caution\n\n${content}\n\n!!!\n\n`
-      }
-    });
-    turndownService.addRule('warnings', {
-      filter: node => {
-        return node.nodeName === 'P' && node.classList.contains('warning');
-      },
-      replacement: (content, node) => {
-        return `!!!.aside.aside--warning\n\n${content}\n\n!!!\n\n`
-      }
-    });
-    turndownService.addRule('h2', {
-      filter: node => {
-        return node.nodeName === 'H2' && node.hasAttribute('id');
-      },
-      replacement: (content, node) => {
-        return `## ${content} {: #${node.id} }\n\n`;
-      }
-    });
-    turndownService.addRule('h3', {
-      filter: node => {
-        return node.nodeName === 'H3' && node.hasAttribute('id');
-      },
-      replacement: (content, node) => {
-        return `### ${content} {: #${node.id} }\n\n`;
-      }
-    });
-    turndownService.addRule('h4', {
-      filter: node => {
-        return node.nodeName === 'H4' && node.hasAttribute('id');
-      },
-      replacement: (content, node) => {
-        return `#### ${content} {: #${node.id} }\n\n`;
-      }
-    });
-    turndownService.addRule('h5', {
-      filter: node => {
-        return node.nodeName === 'H5' && node.hasAttribute('id');
-      },
-      replacement: (content, node) => {
-        return `##### ${content} {: #${node.id} }\n\n`;
-      }
-    });
+    // turndownService.addRule('notes', {
+    //   filter: node => {
+    //     return node.nodeName === 'P' && node.classList.contains('note');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `!!!.aside.aside--note\n\n${content}\n\n!!!\n\n`
+    //   }
+    // });
+    // turndownService.addRule('cautions', {
+    //   filter: node => {
+    //     return node.nodeName === 'P' && node.classList.contains('caution');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `!!!.aside.aside--caution\n\n${content}\n\n!!!\n\n`
+    //   }
+    // });
+    // turndownService.addRule('warnings', {
+    //   filter: node => {
+    //     return node.nodeName === 'P' && node.classList.contains('warning');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `!!!.aside.aside--warning\n\n${content}\n\n!!!\n\n`
+    //   }
+    // });
+    // turndownService.addRule('h2', {
+    //   filter: node => {
+    //     return node.nodeName === 'H2' && node.hasAttribute('id');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `## ${content} {: #${node.id} }\n\n`;
+    //   }
+    // });
+    // turndownService.addRule('h3', {
+    //   filter: node => {
+    //     return node.nodeName === 'H3' && node.hasAttribute('id');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `### ${content} {: #${node.id} }\n\n`;
+    //   }
+    // });
+    // turndownService.addRule('h4', {
+    //   filter: node => {
+    //     return node.nodeName === 'H4' && node.hasAttribute('id');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `#### ${content} {: #${node.id} }\n\n`;
+    //   }
+    // });
+    // turndownService.addRule('h5', {
+    //   filter: node => {
+    //     return node.nodeName === 'H5' && node.hasAttribute('id');
+    //   },
+    //   replacement: (content, node) => {
+    //     return `##### ${content} {: #${node.id} }\n\n`;
+    //   }
+    // });
     // turndownService.addRule('dt', {
     //   filter: 'dt',
     //   replacement: function (content) {
@@ -275,19 +283,20 @@ async function migrate(targets, done) {
     //     return `: ${content}`;
     //   }
     // });
-    const markdown = turndownService.turndown(html);
-    frontmatter += '---\n\n';
-    let output = `${markdown}`;
-    if (config.frontmatter) output = `${frontmatter}${output}`; 
-    const formattedOutput = prettier.format(output, { 
-      parser: 'markdown', 
-      proseWrap: 'always',
-      printWidth: 100
-    });
-    fs.writeFileSync(`${destination}/index.md`, formattedOutput);
-    done += `${target}\n`;
-    if (config.history) fs.writeFileSync('done.txt', done);
+    // const markdown = turndownService.turndown(html);
+    // frontmatter += '---\n\n';
+    // let output = `${markdown}`;
+    // if (config.frontmatter) output = `${frontmatter}${output}`; 
+    // const formattedOutput = prettier.format(output, { 
+    //   parser: 'markdown', 
+    //   proseWrap: 'always',
+    //   printWidth: 100
+    // });
+    // fs.writeFileSync(`${destination}/index.md`, formattedOutput);
+    // done += `${target}\n`;
+    // if (config.history) fs.writeFileSync('done.txt', done);
   }
+  console.log(data);
   await browser.close();
 }
 
